3efbc41f81d4e2793fed4207527a28e9652a9f29
Kaysil <31532733+Kaysil@users.noreply.github.com>
2020-06-25 11:55:18 +0700

    new version (stable)

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-!global.data.CustomAlerts ? global.data.CustomAlerts = {} : "";
-
 var wait = global.nodemodule['wait-for-stuff'];
 var fs = global.nodemodule["fs"];
 var path = global.nodemodule["path"];
@@ -34,7 +32,7 @@ var defaultConfig = {
 		  "when_bot_join_change_nick_name": "[{botprefix}] {botname}",
 		  "when_user_add_bot": "User {username} ({userid}) added me into {threadname} ({threadid})",
 		  "when_user_kick_bot": "User {username} ({userid}) removed me from {threadname} ({threadid})",
-		  "link_line": "[{userlink}, {threadlink}]"
+		  "link_line": "{userlink}\n{threadlink}"
 	 }
 };
 
@@ -47,84 +45,6 @@ if (!fs.existsSync(path.join(rootpath, "config.json"))) {
 	}));
 }
 
-var langMap = {
-	"en_US": {
-		"invalidArgs": "Using: " + global.config.commandPrefix + "cas [message] | reset",
-		"notisGroup": "You can using this command here! Try using in the group chat"
-	},
-	"vi_VN": {
-		"invalidArgs": "Sử dụng: " + global.config.commandPrefix + "cas [tin nhắn] | reset",
-		"notisGroup": "Bạn không thể dùng lệnh này tại đây! Hãy thử dùng nó trong nhóm"
-	}
-}
-
-var getLang = function () {}
-var onLoad = function (data) {
-	var log = data.log;
-	getLang = function (langVal, userID) {
-	  var lang = global.config.language;
-	  if (userID && global.data.userLanguage[userID]) {
-		lang = global.data.userLanguage[userID];
-		if (!langMap[lang]) {
-		  log("Warning: Invalid language: ", lang, `; using ${global.config.language} as fallback...`);
-		  lang = global.config.language;
-		}
-	  }
-
-	  if (langMap[lang]) {
-		return String(langMap[lang][langVal]);
-	  } else {
-		log("Warning: Invalid language: ", lang, "; using en_US as fallback...");
-		return String((langMap["en_US"] || {})[langVal]);
-	  }
-	}
-}
-
-var cascmd = function (type, data) {
-	if (data.msgdata.isGroup) {
-		if (!global.data.CustomAlerts[data.msgdata.threadID]) global.data.CustomAlerts[data.msgdata.threadID] = {};
-		var args = data.args;
-			args.shift();
-		if (!args) {
-			data.return({
-				handler: "internal",
-				data:getLang("invalidArgs", `FB-${data.msgdata.senderID}`)
-			})
-		}
-		var mstr = "";
-		switch (args[0].toLowerCase()) {
-			case "join":
-			case "j":
-				mstr = data.args;
-				mstr.shift()
-				if (mstr === "reset" || mstr === "default") {
-					global.data.CustomAlerts[data.msgdata.threadID].join = config.messages.botJoinMessage;
-				} else if (mstr === "off") {
-					global.data.CustomAlerts[data.msgdata.threadID].join = "off";
-				} else {
-					global.data.CustomAlerts[data.msgdata.threadID].join = mstr.join(" ");
-				}
-				break;
-			case "leave":
-			case "l":
-				mstr = data.args;
-				mstr.shift()
-				if (mstr === "reset" || mstr === "default") {
-					global.data.CustomAlerts[data.msgdata.threadID].leave = config.messages.botLeaveMessage;
-				} else if (mstr === "off") {
-					global.data.CustomAlerts[data.msgdata.threadID].leave = "off";
-				} else {
-					global.data.CustomAlerts[data.msgdata.threadID].leave = mstr.join(" ");
-				}
-		}
-	} else {
-		data.return({
-			handler: "internal",
-			data: getLang("notisGroup", `FB-${data.msgdata.senderID}`)
-		})
-	}
-}
-
 var chathook = function(type, data) {
 	var fb = data.facebookapi;
     var msg = data.msgdata;
@@ -135,22 +55,20 @@ var chathook = function(type, data) {
 if (msg.type === 'event') {
 	switch (msg.logMessageType) {
 		case 'log:subscribe':
-			msg.logMessageData.addedParticipants.forEach(id => {
+			msg.logMessageData.addedParticipants.forEach(user => {
 				
 			var [err, threadInfo] = wait.for.function(data.facebookapi.getThreadInfo, data.msgdata.threadID);
-			var userID = id.userFbId;
+			var userID = user.userFbId;
+			var authorID = msg.author;
 			
-			fb.getUserInfo([userID], (err, userInfo) => {
+			fb.getUserInfo([userID, authorID], (err, userInfo) => {
+				if (err) data.log(err);
+				
 				var userMentions = `@${userInfo[userID].name}`;
-				var join = "";
-
-			if (!global.data.CustomAlerts[msg.threadID].join || global.data.CustomAlerts[msg.threadID].join === "" || global.data.CustomAlerts[msg.threadID].join === config.messages.whenUserJoin) {
-				join = data.prefix + " " + config.messages.whenUserJoin.replace("{username}", userMentions).replace("{groupname}", threadInfo.name).replace("{membercount}", Object.keys(threadInfo.participantIDs).length);
-			} else if (global.data.CustomAlerts[msg.threadID].join === "off") {
-				join = "";
-			} else {
-				join = data.prefix + " " + global.data.CustomAlerts[msg.threadID].join.replace("{username}", userMentions).replace("{groupname}", threadInfo.name).replace("{membercount}", Object.keys(threadInfo.participantIDs).length);
-			}
+				var join = data.prefix + " " + config.messages.whenUserJoin
+				.replace("{username}", userMentions)
+				.replace("{groupname}", threadInfo.name)
+				.replace("{membercount}", Object.keys(threadInfo.participantIDs).length);
 
 			if (userID !== fb.getCurrentUserID()) {
 				fb.sendMessage({
@@ -187,24 +105,21 @@ if (msg.type === 'event') {
 		break;
 		case 'log:unsubscribe':
 			var userID = msg.logMessageData.leftParticipantFbId;
+			var authorID = msg.author;
 			var [err, threadInfo] = wait.for.function(data.facebookapi.getThreadInfo, data.msgdata.threadID);
 			
-			fb.getUserInfo([userID], (err, userInfo) => {
-
+			fb.getUserInfo([userID, authorID], (err, userInfo) => {
+				if (err) data.log(err);
+				
 			var userMentions = `@${userInfo[userID].name}`;
-			var leave = "";
-
-			if (!global.data.CustomAlerts[msg.threadID].leave || global.data.CustomAlerts[msg.threadID].leave === "" || global.data.CustomAlerts[msg.threadID].leave === config.messages.whenUserLeave) {
-				leave = data.prefix + " " + config.messages.whenUserLeave.replace("{username}", userMentions).replace("{groupname}", threadInfo.name).replace("{membercount}", Object.keys(threadInfo.participantIDs).length);
-			} else if (global.data.CustomAlerts[msg.threadID].join === "off") {
-				leave = "";
-			} else {
-				leave = data.prefix + " " + global.data.CustomAlerts[msg.threadID].leave.replace("{username}", userMentions).replace("{groupname}", threadInfo.name).replace("{membercount}", Object.keys(threadInfo.participantIDs).length);
-			}
+			var leave = data.prefix + " " + config.messages.whenUserLeave
+			.replace("{username}", userMentions)
+			.replace("{groupname}", threadInfo.name)
+			.replace("{membercount}", Object.keys(threadInfo.participantIDs).length);
 
 			if (userID !== fb.getCurrentUserID()) {
 				fb.sendMessage({
-					body: `${data.prefix} ${leave}`,
+					body: leave,
 					mentions: [{
 						tag: userMentions,
 						id: userID
@@ -219,8 +134,8 @@ if (msg.type === 'event') {
 				.replace("{userid}", authorID);
 				str += `\n\n`;
 				str += config.messages["link_line"]
-				.replace("{threadlink}", "https://facebook.com/messages/t/" +threadID)
-				.replace("{userlink}", "https://facebook.com/" +authorID);
+				.replace("{threadlink}", "https://facebook.com/messages/t/" + threadID)
+				.replace("{userlink}", "https://facebook.com/" + authorID);
 				getFacebookAdmin().forEach(n => {
 					data.facebookapi.sendMessage(str, n.slice(3));
 				});
@@ -233,7 +148,5 @@ if (msg.type === 'event') {
 }
 
 module.exports = {
-	chathook,
-	cascmd,
-	onLoad
+	chathook
 };
